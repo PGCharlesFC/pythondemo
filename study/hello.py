d512513d695b6aa3b33877afc79dd64a75ba3dcf
Charles <developer_charles@163.com>
2019-12-29 17:11:02 +0800

    print测试

---
+++ study/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.6
+# -*- encoding: utf-8 -*-
+"""
+    @File    : hello.py
+    @Time    : 2019/12/29 下午5:07
+    @Author  : Charles
+    @Version : 1.0
+    @Desc    : None
+"""
+
+
+print("hello, world!")
